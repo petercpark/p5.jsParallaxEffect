4ba5772c041b6459181a085920e65dc4c8e5648b
petercpark <peterchpark@gmail.com>
2018-08-07 14:29:26 -0700

    text

--- sketch.js
+++ sketch.js
@@ -1,25 +1,28 @@
-//parallax mouse
-var fox;
-var bg;
+//parallax p5.js code
+var img = [];
+
+
 
 var xPos;
 var yPos;
-var divider = [5, 20];
+
+//the greater the speed the slower it is
+var speed = [20, 50, 10];
 
 var easing = 0.1;
 var x = [];
 var y = [];
 
 function preload(){
-	fox = loadImage('https://image.ibb.co/ds5trK/fox.png');
-	bg = loadImage('https://raw.githubusercontent.com/petercpark/p5.jsParallaxEffect/master/background.jpg');
+	img[0] = loadImage('https://raw.githubusercontent.com/petercpark/p5.jsParallaxEffect/master/background.jpg');
+	img[1] = loadImage('https://image.ibb.co/ds5trK/fox.png');
 }
 
 function setup(){
 var cnv = createCanvas(windowWidth, windowHeight);
 cnv.style('display', 'block');
 
-for (var i = 0; i < divider.length; i++) {
+for (var i = 0; i < speed.length; i++) {
 	x[i] = 0;
 	y[i] = 0;
 }
@@ -35,13 +38,19 @@ function draw(){
 	easeFunction();
 
 	imageMode(CENTER);
-	image(bg, width/2 + x[1], height/2 + y[1], bg.width * 4/3, bg.height * 4/3);
-	image(fox, width/2 + x[0], height/2 + y[0]);
+	image(img[0], width/2 + x[1], height/2, width + 500, height + 500);
+	image(img[1], width/3 + x[0], height/2 + y[0]);
+
+	textAlign(CENTER);
+	textSize(width/15);
+	fill(255, 220, 200);
+	textFont('Sedgwick Ave Display');
+	text('Parallax p5.js', width * 2/3 + x[2], height/2 + y[2]);
 }
 
 function easeFunction(){
-	for (var i = 0; i < divider.length; i++) {
-	x[i] += (xPos / divider[i] - x[i]) * easing;
-	y[i] += (yPos / divider[i] - y[i]) * easing;
+	for (var i = 0; i < speed.length; i++) {
+	x[i] += (xPos / speed[i] - x[i]) * easing;
+	y[i] += (yPos / speed[i] - y[i]) * easing;
  }
 }
