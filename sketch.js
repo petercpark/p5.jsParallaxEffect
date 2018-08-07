283ba00a91d411517c8b734003bcfaa1a9d16a63
petercpark <peterchpark@gmail.com>
2018-08-07 15:40:05 -0700

    change background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 //parallax p5.js code
 var img = [];
 
-
+var scroll;
 
 var xPos;
 var yPos;
@@ -13,14 +13,16 @@ var easing = 0.1;
 var x = [];
 var y = [];
 
+var cnv;
+
 function preload(){
 	img[0] = loadImage('https://raw.githubusercontent.com/petercpark/p5.jsParallaxEffect/master/background.jpg');
 	img[1] = loadImage('https://image.ibb.co/ds5trK/fox.png');
+	scroll = createImg('https://raw.githubusercontent.com/petercpark/p5.jsParallaxEffect/master/scroll.gif').size(100, 100);
 }
 
 function setup(){
-var cnv = createCanvas(windowWidth, windowHeight);
-cnv.style('display', 'block');
+scroll.style('position', 'absolute');
 
 for (var i = 0; i < speed.length; i++) {
 	x[i] = 0;
@@ -30,6 +32,9 @@ for (var i = 0; i < speed.length; i++) {
 }
 
 function draw(){
+	cnv = createCanvas(windowWidth, windowHeight);
+	cnv.style('display', 'block');
+
 	background(0);
 
 	xPos = (mouseX - width/2);
@@ -46,6 +51,8 @@ function draw(){
 	fill(255, 220, 200);
 	textFont('Sedgwick Ave Display');
 	text('Parallax p5.js', width * 2/3 + x[2], height/2 + y[2]);
+
+	scroll.position(width/2 - 50, height - 100);
 }
 
 function easeFunction(){
@@ -53,4 +60,8 @@ function easeFunction(){
 	x[i] += (xPos / speed[i] - x[i]) * easing;
 	y[i] += (yPos / speed[i] - y[i]) * easing;
  }
+}
+
+function mouseWheel(event){
+	
 }
